cacf48bdf30fb9f9df89b4b88bcf3f80d0bdbe32
Nicholas G. Bodeis <ngbodeis@csis.svsu.edu>
2019-03-11 11:11:14 -0400

    initial commit

---
+++ database.php
@@ -0,0 +1,43 @@
+<?php
+	/* This file allows the webpage to be connected to the database using the saved variables 
+	*/
+
+
+class Database 
+{
+	// these are not the real passwords
+	// for real passwords, see file in ../database subdirectory
+	private static $dbName = 'ngbodeis355wi19' ; 
+	private static $dbHost = '10.8.30.49' ;
+	private static $dbUsername = 'ngbodeis355wi19';
+	private static $dbUserPassword = 'ngb12345!';
+	
+	private static $cont  = null;
+	
+	public function __construct() {
+		exit('Init function is not allowed');
+	}
+	
+	public static function connect()
+	{
+	   // One connection through whole application
+       if ( null == self::$cont )
+       {      
+        try 
+        {
+          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);  
+        }
+        catch(PDOException $e) 
+        {
+          die($e->getMessage());  
+        }
+       } 
+       return self::$cont;
+	}
+	
+	public static function disconnect()
+	{
+		self::$cont = null;
+	}
+}
+?>
